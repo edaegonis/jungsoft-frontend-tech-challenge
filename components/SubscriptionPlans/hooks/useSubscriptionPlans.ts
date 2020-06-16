e13fbf4e9e1c8336abb898fc7b51bcc6b8916a15
Eduardo C. de Souza <eduardofidera@gmail.com>
2020-06-16 00:45:14 -0300

    Add handler for selecting plan parameters

--- components/SubscriptionPlans/hooks/useSubscriptionPlans.ts
+++ components/SubscriptionPlans/hooks/useSubscriptionPlans.ts
@@ -2,8 +2,10 @@ import { useQuery } from "@apollo/react-hooks"
 import { useState } from "react"
 
 import { ALL_PLANS_QUERY } from "../../../queries/subscription-plans"
+import { getArePropertyValuesEqual } from "../../../helpers/functional"
 
 export interface Plan {
+  [key: string]: number | string
   id: number
   numberOfPeople: number
   price: string
@@ -11,26 +13,36 @@ export interface Plan {
 }
 
 export function useSubscriptionPlans() {
-  const [selectedPlanId, setSelectedPlanId] = useState<number | false>(false)
+  const [selectedParamValues, setSelectedParamValues] = useState({})
   const queryResult = useQuery(ALL_PLANS_QUERY)
 
-  function setSelectedSubscriptionPlanId(id: number) {
-    setSelectedPlanId(id)
-  }
-
   function getSelectedSubscriptionPlan(plans: Plan[]) {
-    /** If there is no selection, returns the first plan immediately */
-    if (!selectedPlanId) return plans[0]
+    const keys = Object.keys(selectedParamValues)
 
-    const selected = plans.find(({ id }) => id === selectedPlanId)
+    /** If there is no selection, returns the first plan immediately */
+    if (!keys.length) return plans[0]
+
+    const selected = plans.find((plan) => {
+      return getArePropertyValuesEqual(plan, selectedParamValues)
+    })
 
     return selected
   }
 
+  function handleSetParamValues(e) {
+    const value = e.target.dataset.value
+    const property = e.target.dataset.name
+
+    setSelectedParamValues((paramValues) => ({
+      ...paramValues,
+      [property]: value,
+    }))
+  }
+
   return {
     queryResult,
-    selectedPlanId,
+    selectedParamValues,
+    handleSetParamValues,
     getSelectedSubscriptionPlan,
-    setSelectedSubscriptionPlanId,
   }
 }
