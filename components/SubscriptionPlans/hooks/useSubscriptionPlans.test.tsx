b4691971816694a3bc83a86867cd0c97f8233ce2
Eduardo C. de Souza <eduardofidera@gmail.com>
2020-06-16 02:48:42 -0300

    Move mocked provider to test utils

--- components/SubscriptionPlans/hooks/useSubscriptionPlans.test.tsx
+++ components/SubscriptionPlans/hooks/useSubscriptionPlans.test.tsx
@@ -1,84 +1,14 @@
 import { renderHook, act } from "@testing-library/react-hooks"
-import { MockedProvider } from "@apollo/react-testing"
 
 import { useSubscriptionPlans } from "./useSubscriptionPlans"
-
-import { ALL_PLANS_QUERY } from "../../../queries/subscription-plans"
-
-const mocks = [
-  {
-    request: {
-      query: ALL_PLANS_QUERY,
-    },
-    result: {
-      data: {
-        listPlans: [
-          {
-            id: 1,
-            numberOfPeople: 2,
-            price: "129.99",
-            weeklyRecipes: 4,
-          },
-          {
-            id: 2,
-            numberOfPeople: 3,
-            price: "149.99",
-            weeklyRecipes: 3,
-          },
-          {
-            id: 3,
-            numberOfPeople: 3,
-            price: "179.99",
-            weeklyRecipes: 4,
-          },
-          {
-            id: 4,
-            numberOfPeople: 3,
-            price: "209.99",
-            weeklyRecipes: 5,
-          },
-          {
-            id: 5,
-            numberOfPeople: 4,
-            price: "209.99",
-            weeklyRecipes: 3,
-          },
-          {
-            id: 6,
-            numberOfPeople: 4,
-            price: "249.99",
-            weeklyRecipes: 4,
-          },
-          {
-            id: 7,
-            numberOfPeople: 4,
-            price: "289.99",
-            weeklyRecipes: 5,
-          },
-          {
-            id: 8,
-            numberOfPeople: 4,
-            price: "329.99",
-            weeklyRecipes: 6,
-          },
-        ],
-      },
-    },
-  },
-]
+import { MockedProviderWrapper } from "../../../lib/test-utils"
 
 describe("useSubscriptionPlans", function () {
-  const wrapper = ({ children }) => (
-    <MockedProvider mocks={mocks} addTypename={false}>
-      {children}
-    </MockedProvider>
-  )
-
   /** Test for primitive value */
   describe("queryResult", function () {
     it("should render loading state initially", async () => {
       const { result } = renderHook(() => useSubscriptionPlans(), {
-        wrapper,
+        wrapper: MockedProviderWrapper,
       })
 
       expect(result.current.queryResult.loading).toBeTruthy()
@@ -89,7 +19,7 @@ describe("useSubscriptionPlans", function () {
   describe("getSelectedSubscriptionPlan", function () {
     it("should return the first plan if there is no selection", async () => {
       const { result, wait } = renderHook(() => useSubscriptionPlans(), {
-        wrapper,
+        wrapper: MockedProviderWrapper,
       })
 
       await wait(() => expect(result.current.queryResult.data).toBeTruthy())
@@ -114,7 +44,7 @@ describe("useSubscriptionPlans", function () {
     /** Feature: Change the selected subscription plan */
     it("should return the actual plan if selected", async () => {
       const { result, wait } = renderHook(() => useSubscriptionPlans(), {
-        wrapper,
+        wrapper: MockedProviderWrapper,
       })
 
       await wait(() => expect(result.current.queryResult.data).toBeTruthy())
--- lib/test-utils.tsx
+++ lib/test-utils.tsx
@@ -2,6 +2,76 @@ import "@testing-library/jest-dom/extend-expect"
 import { render } from "@testing-library/react"
 
 import { withApollo } from "./apollo"
+import { ALL_PLANS_QUERY } from "../queries/subscription-plans"
+import { MockedProvider } from "@apollo/react-testing"
+
+const mocks = [
+  {
+    request: {
+      query: ALL_PLANS_QUERY,
+    },
+    result: {
+      data: {
+        listPlans: [
+          {
+            id: 1,
+            numberOfPeople: 2,
+            price: "129.99",
+            weeklyRecipes: 4,
+          },
+          {
+            id: 2,
+            numberOfPeople: 3,
+            price: "149.99",
+            weeklyRecipes: 3,
+          },
+          {
+            id: 3,
+            numberOfPeople: 3,
+            price: "179.99",
+            weeklyRecipes: 4,
+          },
+          {
+            id: 4,
+            numberOfPeople: 3,
+            price: "209.99",
+            weeklyRecipes: 5,
+          },
+          {
+            id: 5,
+            numberOfPeople: 4,
+            price: "209.99",
+            weeklyRecipes: 3,
+          },
+          {
+            id: 6,
+            numberOfPeople: 4,
+            price: "249.99",
+            weeklyRecipes: 4,
+          },
+          {
+            id: 7,
+            numberOfPeople: 4,
+            price: "289.99",
+            weeklyRecipes: 5,
+          },
+          {
+            id: 8,
+            numberOfPeople: 4,
+            price: "329.99",
+            weeklyRecipes: 6,
+          },
+        ],
+      },
+    },
+  },
+]
+
+export const MockedProviderWrapper = ({ children }) => (
+  <MockedProvider mocks={mocks} addTypename={false}>
+    {children}
+  </MockedProvider>
+)
 
 const WrappedWithApollo = withApollo({ ssr: true })(({ children }) => {
   return { children }
